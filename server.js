6ac4308ec8360b114d83d1dfe67834607e67adf2
daruspatron <dumitrescudariusandrei2005@gmail.com>
2025-03-06 22:08:41 +0200

    server.js

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-});
-
 app.use(cors());
 app.use(express.json());  // 🔹 Permite procesarea datelor JSON
 
