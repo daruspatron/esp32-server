bb98369758bd33917fb996ccbb66b15df9e7d693
daruspatron <dumitrescudariusandrei2005@gmail.com>
2025-03-06 22:00:48 +0200

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
